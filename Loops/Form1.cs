91f889e833147346cc6d18d20b079d1d8fcf0013
Kevin <kevingilbert01@hotmail.com>
2019-08-12 11:17:23 -0400

    first commit

---
+++ Loops/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loops {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e) {
+            int answer = 0;
+            int loopStart = int.Parse(textBox1.Text);
+            int loopEnd = int.Parse(textBox2.Text);
+            int multiplyBy = int.Parse(textBox3.Text);
+            listBox1.Items.Clear();
+            for (int i = loopStart; i <= loopEnd; i++) {
+                answer = multiplyBy * i;
+                listBox1.Items.Add(i + " times " + multiplyBy + " = " + answer.ToString());
+            }
+        }
+    }
+}
